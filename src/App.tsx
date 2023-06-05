969466996ca4ca99be842690cae08d99a06d7e9c
Isekai Code <109307799+IsekaiCode@users.noreply.github.com>
2023-06-05 21:45:21 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function Game({timer, count, setCount}) {
-
+function Game({
+  timer,
+  count,
+  setCount,
+}: {
+  timer: number;
+  count: number;
+  setCount: Function;
+}) {
   function handleClick() {
     setCount(count + 1);
   }
@@ -17,39 +24,61 @@ function Game({timer, count, setCount}) {
   );
 }
 
-function Start({ onButtonClick }) {
-    return (
+function Start({
+  timer,
+  onButtonClick,
+}: {
+  timer: string;
+  onButtonClick: any;
+}) {
+  return (
+    <div>
+      <h1>CPS test</h1>
       <div>
-        <h1>CPS test</h1>
-        <button className="btn1" onClick={ onButtonClick } >
-          Click to begin
-        </button>
+        <label>Select the number of seconds: </label>
+        <input placeholder={timer} id="textbox"></input>
       </div>
-    ); 
+      <button className="btn1" onClick={onButtonClick}>
+        Click to begin
+      </button>
+    </div>
+  );
 }
 
 export default function MyApp() {
   const [start, setStart] = useState(false);
-  const [timer, setTimer] = useState(60);
+  const [staringtimer, setStartingTimer] = useState(1);
+  const [timer, setTimer] = useState(1);
   const [count, setCount] = useState(0);
 
+  function onButtonClick() {
+    var inputvalue = parseInt((document.getElementById("textbox") as HTMLInputElement)
+      .value);
+    if (!isNaN(inputvalue)) {
+      setTimer(inputvalue);
+      setStartingTimer(inputvalue);
+    }
+    setStart(true);
+  }
+
   if (start) {
-    if (timer == 0) {
+    if (timer < 1) {
       return (
         <div>
           <h1>GAME OVER</h1>
-          <h1>Your record is {count}</h1>
+          <h1>
+            Your record is <br></br>
+            {(count / staringtimer).toFixed(1)} clicks per second
+          </h1>
         </div>
       );
     } else {
-      setTimeout(() => {setTimer(timer - 1);}, 100);
-      return (
-        <Game timer={timer} count={count} setCount={setCount}/>
-      );
+      setTimeout(() => {
+        setTimer(timer - 1);
+      }, 1000);
+      return <Game timer={timer} count={count} setCount={setCount} />;
     }
   } else {
-    return (
-      <Start onButtonClick={() => setStart(true)}/>
-    );
+    return <Start timer={timer.toString()} onButtonClick={onButtonClick} />;
   }
 }
